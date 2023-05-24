9ebaf13af9cbc724d0f8b117793a55e93370a87a
22020454 <22020454@10.172.150.42>
2023-05-24 12:09:15 +0800

    first push to P00 repository in github

--- P00/test/CalculatorTest.java
+++ P00/test/CalculatorTest.java
@@ -25,65 +25,8 @@ public class CalculatorTest {
 	}
 
 	@Test
-	public void testSubtract() {
-		int a = 9876;
-		int b = 4321;
-		
-		Calculator cal = new Calculator();
-		int actual = cal.subtract(a,b);
-		
-		int expected = 5555;
-		assertEquals(actual,expected);
+	public void test() {
+		fail("Not yet implemented");
 	}
-	
-	@Test
-	public final void testAdd() {
-		int a = 1234;
-		int b = 8765;
-		
-		Calculator cal = new Calculator ();
-		
-		int actual = cal.add(a, b);
-		
-		int expected = 5555;
-		assertEquals(expected, actual);
-		
-	}
-	
-	
-	@Test
-	public final void testMultiple() {
-		int a = 12;
-		int b = 12;
-		
-		Calculator cal = new Calculator ();
-		
-		int actual = cal.multiply(a, b);
-		
-		int expected = 144;
-		
-		if (b == 0) {
-			
-			assertNotEquals("The denomintor cannot be 0", expected, actual);
 
-		}else {
-			
-			assertEquals(expected, actual);
-		}
-		
-	}
-	
-	@Test
-	public final void testDivide() {
-		int a = 12;
-		int b = 6;
-		
-		Calculator cal = new Calculator ();
-		
-		int actual = cal.divide(a, b);
-		
-		int expected = 2;
-		assertEquals(expected, actual);
-		
-	}
 }
